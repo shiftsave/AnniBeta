28dcb415aebc1c5b6923af02b9c867593e933766
shiftsave <ivan@shiftsave.com>
2017-05-17 15:37:51 -0700

    remove quill & implement draftjs

--- src/routes/project/components/Script.js
+++ src/routes/project/components/Script.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
-import ReactQuill from 'react-quill-v2';
+import { EditorState } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+
+import 'draft-js/dist/Draft.css';
 
 class Script extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      editorState: EditorState.createEmpty()
     };
   }
 
+  ComponentDidMount() {
+    this.focus();
+  }
+
+  onChange = (editorState) => {
+    this.setState ({ editorState });
+  }
+
+  focus = () => {
+    this.editor.focus();
+  }
+
   render() {
+    const { editorState } = this.state;
+
     return (
       <div className='Script'>
         <div className="content">
           <h1>Script</h1>
-          <ReactQuill theme="snow"
-            value={this.state.text}
-            onChange={this.onTextChange} />
+          <Editor
+            editorState={editorState}
+            onChange={this.onChange}
+            ref={(element) => { this.editor = element; }}
+            placeholder="Start writing your script..."
+            spellcheck
+          />
         </div>
       </div>
     )
