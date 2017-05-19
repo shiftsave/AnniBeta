0fa1ddaa1acea8e2bc533982935e26c1081b350e
shiftsave <ivan@shiftsave.com>
2017-05-19 12:31:06 -0700

    styles and functional implementation of new editor

--- src/routes/project/components/TextEditor.js
+++ src/routes/project/components/TextEditor.js
@@ -7,27 +7,10 @@ const counterPlugin = createCounterPlugin();
 const { CharCounter, WordCounter, LineCounter, CustomCounter } = counterPlugin;
 const plugins = [counterPlugin];
 
-
-// Custom overrides for "code" style.
-const styleMap = {
-  CODE: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
-};
-
-function getBlockStyle(block) {
-  switch (block.getType()) {
-    case 'blockquote': return 'RichEditor-blockquote';
-    default: return null;
-  }
-}
-
 class StyleButton extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
     this.onToggle = (e) => {
       e.preventDefault();
       this.props.onToggle(this.props.style);
@@ -48,45 +31,17 @@ class StyleButton extends Component {
   }
 }
 
-const BLOCK_TYPES = [
-  {label: 'UL', style: 'unordered-list-item'},
-  {label: 'OL', style: 'ordered-list-item'},
-];
-
-const BlockStyleControls = (props) => {
-  const {editorState} = props;
-  const selection = editorState.getSelection();
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType();
-
-  return (
-    <div>
-      {BLOCK_TYPES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
-};
-
-var INLINE_STYLES = [
+const styleTypes = [
   {label: 'Bold', style: 'BOLD'},
   {label: 'Italic', style: 'ITALIC'},
   {label: 'Underline', style: 'UNDERLINE'},
 ];
 
 const InlineStyleControls = (props) => {
-  var currentStyle = props.editorState.getCurrentInlineStyle();
+  const currentStyle = props.editorState.getCurrentInlineStyle();
   return (
-    <div>
-      {INLINE_STYLES.map(type =>
+    <div className="styles">
+      {styleTypes.map(type =>
         <StyleButton
           key={type.label}
           active={currentStyle.has(type.style)}
@@ -108,43 +63,33 @@ export default class TextEditor extends Component {
     this.focus = () => this.refs.editor.focus();
     this.onChange = (editorState) => this.setState({editorState});
 
-    this.handleKeyCommand = (command) => this._handleKeyCommand(command);
-    this.onTab = (e) => this._onTab(e);
-    this.toggleBlockType = (type) => this._toggleBlockType(type);
-    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
-  }
-
-  _handleKeyCommand(command) {
-    const {editorState} = this.state;
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
+    this.toggleBlockType = (blockType) => {
+      this.onChange(
+        RichUtils.toggleBlockType(
+          this.state.editorState,
+          blockType
+        )
+      );
     }
-    return false;
-  }
 
-  _onTab(e) {
-    const maxDepth = 4;
-    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
-  }
+    this.toggleInlineStyle = (inlineStyle) => {
+      this.onChange(
+        RichUtils.toggleInlineStyle(
+          this.state.editorState,
+          inlineStyle
+        )
+      );
+    }
 
-  _toggleBlockType(blockType) {
-    this.onChange(
-      RichUtils.toggleBlockType(
-        this.state.editorState,
-        blockType
-      )
-    );
-  }
-
-  _toggleInlineStyle(inlineStyle) {
-    this.onChange(
-      RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        inlineStyle
-      )
-    );
+    this.handleKeyCommand = (command) => {
+      const {editorState} = this.state;
+      const newState = RichUtils.handleKeyCommand(editorState, command);
+      if (newState) {
+        this.onChange(newState);
+        return true;
+      }
+      return false;
+    }
   }
 
   customCountFunction(str) {
@@ -170,22 +115,11 @@ export default class TextEditor extends Component {
         <h1>Script</h1>
         <div className='content'>
           <div className='Editor' onClick={this.focus}>
-            <BlockStyleControls
-              editorState={editorState}
-              onToggle={this.toggleBlockType}
-            />
-            <InlineStyleControls
-              editorState={editorState}
-              onToggle={this.toggleInlineStyle}
-            />
             <div className={className} onClick={this.focus}>
               <Editor
-                blockStyleFn={getBlockStyle}
-                customStyleMap={styleMap}
                 editorState={editorState}
                 handleKeyCommand={this.handleKeyCommand}
                 onChange={this.onChange}
-                onTab={this.onTab}
                 placeholder="Tell a story..."
                 plugins={plugins}
                 ref="editor"
@@ -194,12 +128,15 @@ export default class TextEditor extends Component {
             </div>
           </div>
           <div className="Toolbar">
-            <div><CharCounter limit={200} /> characters</div>
-            <div><WordCounter limit={30} /> words</div>
-            <div><LineCounter limit={10} /> lines</div>
-            <div>
-              <CustomCounter limit={40} countFunction={this.customCountFunction} />
-              <span> words (custom function)</span>
+            <InlineStyleControls
+              editorState={editorState}
+              onToggle={this.toggleInlineStyle}
+            />
+            <div className="stats">
+              <div><CharCounter /> characters</div>
+              <div><WordCounter /> words</div>
+              <div><LineCounter /> lines</div>
+              <div><CustomCounter countFunction={this.customCountFunction} /> words (custom function) </div>
             </div>
           </div>
         </div>
