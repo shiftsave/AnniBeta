26dbe3f5ef56712ddab051fc1430b6d7d01a01f6
shiftsave <ivan@shiftsave.com>
2017-05-29 23:56:41 -0700

    fix merge issues

--- src/components/baseline/index.js
+++ src/components/baseline/index.js
@@ -6,4 +6,5 @@ export { Button } from './components/Button'
 export { ButtonGroup } from './components/ButtonGroup'
 export { Card } from './components/Card'
 export { Icon } from './components/Icon';
+export { Loader } from './components/Loader';
 export { TextArea } from './components/TextArea';
