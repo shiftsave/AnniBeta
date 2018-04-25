4e3a8d7e2b8405e3aefb42e852aa2b1a18cab811
shiftsave <ivan@shiftsave.com>
2018-04-24 21:16:26 -0500

    add jest testing suite

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,10 @@
 /* config-overrides.js */
+const rewireStyledComponents = require("react-app-rewire-styled-components");
 
 module.exports = function override(config, env) {
-  const rewireStyledComponents = require("react-app-rewire-styled-components");
-
-  /* config-overrides.js */
-  module.exports = function override(config, env) {
-    config = rewireStyledComponents(config, env, {
-      ssr: true,
-      displayName: true
-    });
-    return config;
-  };
+  config = rewireStyledComponents(config, env, {
+    ssr: true,
+    displayName: true
+  });
+  return config;
 };
